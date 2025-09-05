01cb309209f3468170cc7f5239fc7bace8082fe1
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-05 10:16:48 +0200

    register form with full styling

--- index.php
+++ index.php
@@ -17,6 +17,21 @@
         <p>Don't have an account? <a href="#">Register</a></p>
       </p>
     </div>
+    <div class="form-box" id="register-form">
+      <p action="">
+        <h2>Register</h2>
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <select name="role" required>
+          <option value="">--Select Role--</option>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+        <button type="submit" name="register">Register</button>
+        <p>Already have an account? <a href="#">Login</a></p>
+      </p>
+    </div>
   </div>
 </body>
 </html>
