eaad88cf361d06d69793d8de55b032868843c2f8
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-05 09:26:16 +0200

    initial setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Full Stack PHP Login and Register Page With User and Admin Page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Login</h1>
+</body>
+</html>
