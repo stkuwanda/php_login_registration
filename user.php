a5912ef68933b451aa6330d4708caa7ac114f8a3
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-07 06:07:16 +0200

    user page completion, style updates for user and admin pages

---
+++ user.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['email'])){
+  header("Location: index.php");
+  exit();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>User Page</title>
+</head>
+<body class="bg-light">
+  <div class="box">
+    <h1>Welcome, <span><?= $_SESSION['name'] ?></span></h1>
+    <p>This is an <span>user</span> page</p>
+    <button onclick="window.location.href='logout.php'">Logout</button>
+  </div>
+</body>
+</html>
